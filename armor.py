7a49ed7696b84a01a2c696acccc6cbcee8b65372
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2024-11-18 21:43:02 -0800

    added armor

---
+++ armor.py
@@ -0,0 +1,8 @@
+class Armor:
+  def __init__(self, name, max_block):
+    '''Instantiate instance properties.
+        name: String
+        max_block: Integer
+    '''
+    # TODO: Create instance variables for the values passed in.
+    pass
