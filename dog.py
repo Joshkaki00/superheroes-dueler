7f496a09f337318e2ad0d639ae4a5681cdfbe344
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2024-11-18 16:23:57 -0800

    init superhero dueler

--- dog.py
+++ dog.py
@@ -1,11 +1,9 @@
 # dog.py
 class Dog:
-  def __init__(self, name):
+  def __init__(self, name, breed):
     self.name = name
+    self.breed = breed
     print("dog initialized!")
 
-# the same instantiation call that creates a Dog object,
-# but now we've assigned it to the value of the my_dog variable
-my_dog = Dog("Rex")
-print(my_dog)
-print(my_dog.name)
+my_dog = Dog("Rex", "SuperDog")
+print(my_dog.breed)
